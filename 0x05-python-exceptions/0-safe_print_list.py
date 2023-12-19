f4e7e8edd8b05f55b30190a6a0ce665ae3f5cfe7
God'spower Agboola <godpoweragboola@gmail.com>
2023-12-18 20:27:45 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    "prints x elements of a list"
-    i = pichu = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end="")
-            pichu += 1
-        except:
-            continue
-    print()
-    return pichu
+    try:
+        count = 0
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end='')
+            count += 1
+        print("")
+    except IndexError:
+        print("")
+    return (count)
