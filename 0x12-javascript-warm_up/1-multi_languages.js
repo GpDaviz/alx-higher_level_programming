1ffcaf44f9c1c86381e12824da026cdb74272e5f
God'spower Agboola <godpoweragboola@gmail.com>
2024-03-11 16:42:33 -0700

    Task

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
-// script that prints 3 lines
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+let myVar = 'C is fun';
+console.log(myVar);
+myVar = 'Python is cool';
+console.log(myVar);
+myVar = 'JavaScript is amazing';
+console.log(myVar);
