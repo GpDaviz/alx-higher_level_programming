23bba66258f78efa05770ea106496b4f2e11c685
God'spower Agboola <godpoweragboola@gmail.com>
2024-03-12 18:28:15 -0700

    All Task Done

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
-
-  exports.nbOccurences = function (list, searchElement) {
-  return list.filter(item => item === searchElement).length;
+exports.nbOccurences = function (list, searchElement) {
+  let pichu = 0;
+  for (const i in list) {
+    if (list[i] === searchElement) {
+      pichu++;
+    }
+  }
+  return pichu;
 };
